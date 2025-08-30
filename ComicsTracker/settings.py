0c0dd9c62b0ab130851f5ac8b675f1751e5e15ca
Johnathan Burgess <ryanburgess173@outlook.com>
2025-08-30 11:44:37 -0400

    setup db and home page basic template and view

--- ComicsTracker/settings.py
+++ ComicsTracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ComicsTracker',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ComicsTracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'ComicsTracker' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
