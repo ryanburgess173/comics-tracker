b06e53d7394fb42d6d1bcebf83028d803aec87d7
Johnathan Burgess <ryanburgess173@outlook.com>
2026-01-11 12:30:32 -0500

    development code

---
+++ frontend/src/app/pages/find-comics/find-comics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-find-comics',
+    imports: [],
+    templateUrl: './find-comics.component.html',
+    styleUrl: './find-comics.component.scss',
+})
+export class FindComicsComponent{
+}
